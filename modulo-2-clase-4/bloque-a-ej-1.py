8c23dd6e2442447505b9d554a2a02bfbb7b1c412
Roberto Arce <robertoarcee@gmail.com>
2024-10-22 18:10:14 -0300

    fix: ejercicios de la clase pasada

--- modulo-2-clase-4/bloque-a-ej-1.py
+++
@@ -1,7 +0,0 @@
-# Función lambda que retorna el valor absoluto de un número
-valor_absoluto = lambda x: x if x >= 0 else -x
-
-# Ejemplos de uso
-print(valor_absoluto(5))
-print(valor_absoluto(-10))
-print(valor_absoluto(0))
